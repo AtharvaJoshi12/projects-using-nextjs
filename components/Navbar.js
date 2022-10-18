3fd14b9f1c206cac4a3153efde87f66ce3eb2988
Atharva <atharvajoshi202@gmail.com>
2022-10-19 00:22:35 +0530

    Active link added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import React from "react";
-
+import { useRouter } from "next/router";
 function Navbar() {
+  const router = useRouter();
   return (
     <div>
       <div class="drop-shadow-lg list-none flex flex-col w-[60%] text-center rounded-2xl m-auto mt-10 bg-white p-0 sm:flex-row sm:justify-around sm:w-[60%] lg:w-[35%]">
         <Link href="/">
-          <a>
+          <a className={router.pathname == "/" ? "active" : ""}>
             <li class="p-5 hover:cursor-pointer hover:text-purple-800 hover:underline underline-offset-8 decoration-2 font-semibold tracking-widest">
               All
             </li>
@@ -16,7 +17,7 @@ function Navbar() {
         <hr />
 
         <Link href="/reactjs">
-          <a>
+          <a className={router.pathname == "/reactjs" ? "active" : ""}>
             <li class="p-5 hover:cursor-pointer hover:text-purple-800 hover:underline underline-offset-8 decoration-2 font-semibold tracking-widest">
               React
             </li>
@@ -26,7 +27,7 @@ function Navbar() {
         <hr />
 
         <Link href="/nextjs">
-          <a>
+          <a className={router.pathname == "/nextjs" ? "active" : ""}>
             <li class="p-5 hover:cursor-pointer hover:text-purple-800 hover:underline underline-offset-8 decoration-2 font-semibold tracking-widest">
               Next Js
             </li>
@@ -36,7 +37,7 @@ function Navbar() {
         <hr />
 
         <Link href="/normal">
-          <a>
+          <a className={router.pathname == "/normal" ? "active" : ""}>
             <li class="p-5 hover:cursor-pointer hover:text-purple-800 hover:underline underline-offset-8 decoration-2 font-semibold tracking-widest">
               Normal
             </li>
