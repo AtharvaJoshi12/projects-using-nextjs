475b8634a784093e8bdef2be9d9802fe04d914da
Atharva <atharvajoshi202@gmail.com>
2022-10-19 20:35:49 +0530

    Animation added.

--- components/MyHead.js
+++ components/MyHead.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Image from "next/image";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 const MyHead = () => {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, I am Atharva Joshi",
+      "<GuyWhoLovesToCode />",
+      "These Are My Projects",
+      "Using React",
+      "Using Next-Js",
+      "Using HTML,CSS,JavaScript",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
     <div>
       <img
         class="h-52 mt-10 rounded-full m-auto drop-shadow-lg"
         src="../images/myimage.jpg"
         alt="My Image"
+        a
       />
       <h1 class="text-center font-semibold tracking-widest text-2xl mt-5">
-        Hi I am Atharva Joshi
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
       </h1>
     </div>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,28 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+
 function Navbar() {
   const router = useRouter();
   return (
     <div>
-      <div class="drop-shadow-lg list-none flex flex-col w-[60%] text-center rounded-2xl m-auto mt-10 bg-white p-0 sm:flex-row sm:justify-around sm:w-[60%] lg:w-[35%]">
+      <motion.div
+        initial={{
+          y: 0,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        class="drop-shadow-lg list-none flex flex-col w-[60%] text-center rounded-2xl m-auto mt-10 bg-white p-0 sm:flex-row sm:justify-around sm:w-[60%] lg:w-[35%]"
+      >
         <Link href="/">
           <a className={router.pathname == "/" ? "active" : ""}>
             <li class="p-5 hover:cursor-pointer hover:text-purple-800 hover:underline underline-offset-8 decoration-2 font-semibold tracking-widest">
@@ -43,7 +60,7 @@ function Navbar() {
             </li>
           </a>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 }
